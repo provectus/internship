60416f0c19e337d1943ae94f9fed2adec8d77274
akastav <switch@bk.ru>
2021-02-24 12:30:43 +0300

    failed changes commit, revert me

--- devops/03 - docker-compose/example/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class SignupForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
-
