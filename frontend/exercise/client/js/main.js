36147621aa4610ed327671cee2e2dc0d926a562a
Dekshut <denismaloffev@gmail.com>
2021-12-13 11:48:41 +0200

    updated project

--- frontend/exercise/client/js/main.js
+++ frontend/exercise/client/js/main.js
@@ -1,4 +1,11 @@
 "use strict";
 
-
+document.body.onload = function () {
+  setTimeout(() => {
+    const preloader = document.getElementById('page-preloader');
+    if (!preloader.classList.contains('done')) {
+      preloader.classList.add('done');
+    }
+  }, 1000);
+};
 
